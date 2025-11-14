a36a44dff5c748b2c4603e992ab8afd693ad3c04
vvvDuB <atefjmai99@gmail.com>
2025-11-14 01:01:46 +0100

    feat: raw mode

--- kilo.c
+++ kilo.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 
 int main(void) {
-    printf("CCC");
+    char c;
+    /* This while keep read from the stdin and print the char */
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (c != '\n')
+            printf("Char: %c\n", c);
+    }
     return 0;
 }
